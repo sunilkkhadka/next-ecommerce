3edba51144de069b9f7d0fb2b428af73547822e4
sunil khadka <sabin411@sabins-MacBook-Air.local>
2025-01-04 15:44:02 +0545

    feat: Created dev env

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default function HomePage() {
   return (
---
+++ lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Dummy Commerce";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Find your fit";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+export const BACKEND_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000";
