7180418f5c80290cdf17fc17ce86052e87190b1e
sunil khadka <sabin411@sabins-MacBook-Air.local>
2025-01-04 20:55:45 +0545

    feat: Added not found and loading pages

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { Move, MoveRight } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+function NotFoundPage() {
+  return (
+    <div className="w-full h-screen flex flex-col gap-6 items-center justify-center">
+      <h1>404 Page Not Found</h1>
+      <p>
+        <Button asChild variant="outline">
+          <Link href="/" className=" ">
+            Back to home page <MoveRight />
+          </Link>
+        </Button>
+      </p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
